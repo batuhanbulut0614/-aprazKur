2155d860ce9311cc36af881cfd07d2bf2b348377
batuhanbulut0614 <113903075+batuhanbulut0614@users.noreply.github.com>
2022-09-28 12:00:21 +0300

    Initial commit

---
+++ src/çaprazKur/Main.java
@@ -0,0 +1,16 @@
+package çaprazKur;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double dolar=18.45;
+		double euro=17.89;
+		double dovizMiktar=10;
+		String dovizTur ="dolar";
+		if (dovizTur == "dolar") {
+		   double tlMiktar=dolar*dovizMiktar;
+		   
+       
+	}
+
+}
